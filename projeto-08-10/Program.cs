14adbe7b716ebf405519df6eaabb9dae56ad7887
natancajaiba2 <natan.cajaiba@etec.sp.gov.br>
2024-10-08 21:48:57 -0300

    conectando um banco de dados mysql no meu projeto

--- projeto-08-10/Program.cs
+++ projeto-08-10/Program.cs
@@ -3,13 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using MySql.Data;
+using MySql.Data.MySqlClient;
 namespace projeto_08_10
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            MySqlConnection conexao;
+            MySqlCommand cmd;
+            conexao = new MySqlConnection("server=localhost; Database=adega; Uid = root; pwd = ''");
+
+            try
+            {
+                conexao.Open();
+                Console.WriteLine("conectado");
+                
+            }
+            catch (Exception ex){
+                Console.WriteLine(ex.Message);
+            }
+
+            string sql;
+            sql = "select * from bebidas";
+            cmd = new MySqlCommand(sql,conexao);
+            MySqlDataReader rdr;
+            rdr = cmd.ExecuteReader();
+
+            while (rdr.Read()) { 
+            Console.WriteLine("id da bebida:" + rdr["cod_bebida"] + "\n");
+            Console.WriteLine("nome da bebida:" + rdr["nome_bebida"] + "\n tipo da bebida:" + rdr["tipo_bebida"] +"\n");
+            
+            }
+            Console.ReadKey();
+
         }
     }
 }
